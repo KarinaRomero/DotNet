6ba0c272c4791e8f4b04cb71f0f3fca6d09525c8
Karina Romero <betzy_skamm@hotmail.com>
2019-08-31 19:14:16 -0700

    oriented object programming base knowledge

---
+++ CoreSchool/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CoreSchool
+{
+    class School
+    {
+        public string name;
+        public string adress;
+        public int yearEstablish;
+        public string ceoName;
+
+        public void Stamp()
+        {
+            //Todo 
+            Console.Beep(30000, 3);
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var school = new School();
+            school.name = "MySchoolName";
+            school.adress = "number 4, Privet Drive";
+            school.yearEstablish = 1981;
+
+            Console.WriteLine(school.name + "Is Dinging");
+            school.Stamp();
+        }
+    }
+}
