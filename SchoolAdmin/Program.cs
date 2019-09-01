53b049ab7dcc9fba81d03ee78e2ef696d7d63d6f
Karina Romero <betzy_skamm@hotmail.com>
2019-09-01 13:14:43 -0700

    loops

--- SchoolAdmin/Program.cs
+++ SchoolAdmin/Program.cs
@@ -12,40 +12,62 @@ namespace SchoolAdmin
             school.City = "Edinburgh";
             school.Type = SchoolType.Preescolar;
 
-            var school1 = new School("HowartsTWO", 1983, SchoolType.Secundaria, city:"Scotland");
+            var school1 = new School("HowartsTWO", 1983, SchoolType.Secundaria, city: "Scotland");
 
             var courses = new Course[3];
 
-            courses[0] = new Course(){
+            courses[0] = new Course()
+            {
                 Name = "Languaje"
             };
-            
-            courses[1] = new Course(){
+
+            courses[1] = new Course()
+            {
                 Name = "Math"
             };
 
-            courses[2] = new Course(){
+            courses[2] = new Course()
+            {
                 Name = "History"
             };
 
-            
             Console.WriteLine("----------------------School------------------");
             Console.WriteLine(school.ToString());
             Console.WriteLine("----------------------School------------------");
             Console.WriteLine(school1.ToString());
-            Console.WriteLine("");
 
             PrintCourse(courses);
-            
+
         }
         private static void PrintCourse(Course[] arrayCourses)
         {
             int counter = 0;
-
-            while(counter < arrayCourses.Length)
+            // While
+            Console.WriteLine("\nWhile");
+            while (counter < arrayCourses.Length)
             {
                 Console.WriteLine(arrayCourses[counter].ToString());
-                counter ++;
+                counter++;
+            }
+            // Do-While
+            Console.WriteLine("\nDo-While");
+            counter = 0;
+            do
+            {
+                Console.WriteLine(arrayCourses[counter].ToString());
+                counter++;
+            } while (counter < arrayCourses.Length);
+            // For
+            Console.WriteLine("\nFor");
+            for (int i = 0; i < arrayCourses.Length; i++)
+            {
+                Console.WriteLine(arrayCourses[i].ToString());
+            }
+            // For-Each
+            Console.WriteLine("\nFor-Each");
+            foreach (var course in arrayCourses)
+            {
+                Console.WriteLine(course.ToString());
             }
         }
     }
